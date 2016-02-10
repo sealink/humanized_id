cbb10f4779ec39233de3583eb1e0c1b0689891fb
Akil Madan <akil.madan@outlook.com>
2016-02-11 10:28:08 +1030

    Specs.

---
+++ spec/humanized_id/humanizer_spec.rb
@@ -0,0 +1,59 @@
+require 'spec_helper'
+
+describe HumanizedId::Humanizer do
+  let(:humanizer) { HumanizedId::Humanizer.new params }
+
+  context 'when generating humanized id' do
+    let(:generated_id) { humanizer.generate_humanized_id }
+
+    context 'testing various ids' do
+      context 'with human friendly characters' do
+        let(:params) { { id: 'CDFGHKMN' } }
+        it 'should return original string' do
+          expect(generated_id).to eq params[:id]
+        end
+      end
+
+      context 'with non human friendly characters' do
+        let(:params) { { id: '22FARTYFART' } }
+        it 'should return a different humanized id of same size' do
+          expect(generated_id).not_to eq params[:id]
+          expect(generated_id.length).to eq params[:id].length
+        end
+      end
+
+      context 'with numerical id' do
+        let(:params) { { id: 123_456_789 } }
+        it 'should return a sanitised id of same size and structure' do
+          expect(generated_id).to match(/.234.67.9/)
+          expect(generated_id.length).to eq params[:id].to_s.length
+        end
+      end
+    end
+
+    context 'testing various lengths' do
+      let(:params) { { id: 'CDFGHKMN' } } # Human-friendly id, to isolate length testing
+      context 'with greater length' do
+        let(:params) { super().merge(length: 15) }
+        it 'should add additional padding to meet required length' do
+          expect(generated_id.length).to eq(params[:length])
+          expect(generated_id.slice((params[:length] - params[:id].length)..(params[:length] - 1)))
+            .to eq params[:id]
+        end
+      end
+      context 'with smaller length' do
+        let(:params) { super().merge(length: 2) }
+        it 'should trim the id to required length' do
+          expect(generated_id).to eq(params[:id].slice(0..(params[:length] - 1)))
+        end
+      end
+    end
+
+    context 'with prefix added' do
+      let(:params) { { id: 'CDFGHKMN', prefix: 'abc' } }
+      it 'should generate id with prefix added' do
+        expect(generated_id).to eq(params[:prefix] + params[:id])
+      end
+    end
+  end
+end
---
+++ spec/humanized_id_spec.rb
@@ -0,0 +1,53 @@
+require 'spec_helper'
+
+describe HumanizedId do
+  context 'when calling humanize' do
+    let(:params) { { id: '22FARTYFART' } }
+    let(:humanized_id) { HumanizedId.humanize params }
+
+    context 'with minimum required values' do
+      it 'should return a different humanized id of same size' do
+        expect(humanized_id).not_to eq params[:id]
+        expect(humanized_id.length).to eq params[:id].length
+      end
+    end
+    context 'with all params passed in' do
+      let(:params) {
+        {
+          id: '22FARTYFART',
+          length: 20,
+          prefix: 'test'
+        }
+      }
+      it 'should humanize id with appropriate length and prefix' do
+        expect(humanized_id).not_to eq(params[:prefix] + params[:id])
+        expect(humanized_id.size).to eq(params[:prefix].length + params[:length])
+        expect(humanized_id.slice(0..(params[:prefix].length - 1))).to eq params[:prefix]
+      end
+    end
+  end
+
+  context 'when calling generate_random' do
+    let(:params) { {} }
+    let(:random_id) { HumanizedId.generate_random params }
+
+    context 'with minimum required values' do
+      it 'should produce random id of default length' do
+        expect(random_id.length).to eq HumanizedId::DEFAULT_GENERATION_LENGTH
+      end
+    end
+
+    context 'with all values passed in' do
+      let(:params) {
+        {
+          prefix: 'test',
+          length: 3
+        }
+      }
+      it 'should produce random id with requested length and prefix' do
+        expect(random_id.length).to eq(params[:prefix].length + params[:length])
+        expect(random_id[0..(params[:prefix].length - 1)]).to eq params[:prefix]
+      end
+    end
+  end
+end
