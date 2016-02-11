b18a465c0c6654ff23124a51c37af9c8d3cdb8a2
Alessandro Berardi <berardialessandro@gmail.com>
2016-02-12 10:06:23 +1030

    Removes unnecessary arguments in private methods

--- lib/humanized_id/humanizer.rb
+++ lib/humanized_id/humanizer.rb
@@ -20,21 +20,19 @@ module HumanizedId
 
     private
 
-    def convert_to_target_base(id: @id, target_base: @target_charset.length)
-      id.to_s(target_base)
-    rescue ArgumentError
-      raise Error, 'id is not an integer'
+    def convert_to_target_base(id:)
+      fail Error, 'id is not an integer' unless id.is_a? Integer
+      id.to_s(@target_charset.length)
     end
 
-    def convert_to_target_charset(
-      id: @id, target_charset: @target_charset, source_charset: @source_charset)
-      source_charset_subset = source_charset.slice(0, target_charset.length)
-      id.tr(source_charset_subset, target_charset)
+    def convert_to_target_charset(id:)
+      source_charset_subset = @source_charset.slice(0, @target_charset.length)
+      id.tr(source_charset_subset, @target_charset)
     end
 
-    def resize(id: @id, min_length: @min_length, target_charset: @target_charset)
-      if min_length > id.length
-        padding = target_charset[0] * (min_length - id.length)
+    def resize(id:)
+      if @min_length > id.length
+        padding = @target_charset[0] * (@min_length - id.length)
         id = "#{padding}#{id}"
       end
       id
