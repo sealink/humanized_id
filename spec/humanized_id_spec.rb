0328694b5e672a70d5621e0f300251bf07cc187c
Michael Noack <michael@noack.com.au>
2016-02-11 14:30:58 +1030

    Add more explicit tests

--- spec/humanized_id_spec.rb
+++ spec/humanized_id_spec.rb
@@ -20,6 +20,7 @@ describe HumanizedId do
         }
       }
       it 'should humanize id with appropriate length and prefix' do
+        expect(humanized_id).to match /#{params[:prefix]}#{HumanizedId::CHARACTERSET}{20}/
         expect(humanized_id).not_to eq(params[:prefix] + params[:id])
         expect(humanized_id.size).to eq(params[:prefix].length + params[:length])
         expect(humanized_id.slice(0, params[:prefix].length)).to eq params[:prefix]
@@ -47,6 +48,7 @@ describe HumanizedId do
       it 'should produce random id with requested length and prefix' do
         expect(random_id.length).to eq(params[:prefix].length + params[:length])
         expect(random_id[0..(params[:prefix].length - 1)]).to eq params[:prefix]
+        expect(random_id).to match /#{params[:prefix]}#{HumanizedId::CHARACTERSET}{3}/
       end
     end
   end
