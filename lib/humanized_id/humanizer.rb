6e950a5111192d5c9fd19af3810ead2e34315222
Akil Madan <akil.madan@outlook.com>
2016-02-11 16:28:50 +1030

    Implementation change of humanizer for a more specific scope.

--- lib/humanized_id/humanizer.rb
+++ lib/humanized_id/humanizer.rb
@@ -1,39 +1,41 @@
+# frozen_string_literal: true
 module HumanizedId
   class Humanizer
+    SOURCE_CHARSET = %w(0123456789abcdefghijklmnopqrstuvwxyz).freeze
+
     def initialize(id:, length: nil, prefix: '')
       @id               = id
-      @length           = length || id.to_s.length
+      @length           = length
       @prefix           = prefix.nil? ? '' : prefix
-      @target_charset   = HumanizedId::CHARACTERSET
+      @target_charset   = HumanizedId::CHARACTERSET.join('')
+      @source_charset   = SOURCE_CHARSET.join('')
     end
 
     def generate_humanized_id
-      new_id = translate_id_to_charset
-      new_id = resize(id: new_id)
+      new_id = convert_to_target_base id: @id
+      new_id = convert_to_target_charset id: new_id
+      new_id = resize id: new_id if @length
       "#{@prefix}#{new_id}"
     end
 
     private
 
-    def translate_id_to_charset(id: @id, target_charset: @target_charset)
-      new_id = []
-      id.to_s.each_char do |char|
-        new_id << replace_with_compliant_char(char: char, target_charset: target_charset)
-      end
-      new_id.join('')
+    def convert_to_target_base(id: @id, target_base: @target_charset.length)
+      id.to_s(target_base)
+    rescue ArgumentError
+      raise Error, 'id is not an integer'
     end
 
-    def replace_with_compliant_char(char:, target_charset: @target_charset)
-      return char if target_charset.include? char
-      target_charset[SecureRandom.random_number(target_charset.size)]
+    def convert_to_target_charset(
+      id: @id, target_charset: @target_charset, source_charset: @source_charset)
+      source_charset_subset = source_charset.slice(0, target_charset.length)
+      id.tr(source_charset_subset, target_charset)
     end
 
-    def resize(id: @id, required_length: @length)
+    def resize(id: @id, required_length: @length, target_charset: @target_charset)
       if required_length > id.length
-        # Pad with random humanized characters
-        id = "#{HumanizedId.generate_random(length: (required_length - id.length))}#{id}"
-      elsif required_length < id.length
-        id = id.slice(0,required_length)
+        padding = target_charset[0] * (required_length - id.length)
+        id = "#{padding}#{id}"
       end
       id
     end
