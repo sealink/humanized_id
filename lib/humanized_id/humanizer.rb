dd54a37be79c5f9bf307c471c7f5fb7cf5b20fdb
Alessandro Berardi <berardialessandro@gmail.com>
2016-02-12 13:20:23 +1030

    Makes code more readable

--- lib/humanized_id/humanizer.rb
+++ lib/humanized_id/humanizer.rb
@@ -12,24 +12,23 @@ module HumanizedId
     end
 
     def generate_humanized_id
-      commands = %i(convert_to_target_base convert_to_target_charset resize)
-      new_id = commands.inject(@id) { |previous_id, command| method(command).call(id: previous_id) }
+      new_id = resize convert_to_target_charset convert_to_target_base(@id)
       "#{@prefix}#{new_id}"
     end
 
     private
 
-    def convert_to_target_base(id:)
+    def convert_to_target_base(id)
       fail Error, 'id is not an integer' unless id.is_a? Integer
       id.to_s(@target_charset.length)
     end
 
-    def convert_to_target_charset(id:)
+    def convert_to_target_charset(id)
       source_charset_subset = @source_charset.slice(0, @target_charset.length)
       id.tr(source_charset_subset, @target_charset)
     end
 
-    def resize(id:)
+    def resize(id)
       return id if @min_length.nil? || @min_length <= id.length
       padding = @target_charset[0] * (@min_length - id.length)
       "#{padding}#{id}"
